9f5e0dc8ab1db89f87b67fbf333091b184f93659
Siddhi Prajapati <siddhi20045@gmail.com>
2025-05-26 16:10:34 -0400

    project include

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ const toggleBtn = document.getElementById('toggleTheme');
       document.body.classList.toggle('light-mode');
       toggleBtn.textContent = document.body.classList.contains('light-mode') ? 'Dark Mode' : 'Light Mode';
     });
+
+//  contact form
+  const contactForm = document.getElementById("contactForm");
+  const successMsg = document.getElementById("successMsg");
+
+  contactForm.addEventListener("submit", function (e) {
+    e.preventDefault(); // prevent default
+    
+    // Submit via JS
+    const formData = new FormData(contactForm);
+    fetch(contactForm.action, {
+      method: "POST",
+      body: formData,
+      headers: { 'Accept': 'application/json' }
+    }).then(response => {
+      if (response.ok) {
+        contactForm.reset();
+        successMsg.classList.remove("d-none");
+      } else {
+        alert("Form submission failed. Try again.");
+      }
+    }).catch(error => {
+      alert("Something went wrong.");
+    });
+  });
+
